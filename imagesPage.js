f888261438fb749ee569fac360bbaae790a72bcc
Julie <vogelmanj@gmail.com>
2012-12-02 10:30:35 -0800

    fixed Los Banos error

--- imagesPage.js
+++ imagesPage.js
@@ -1,7 +1,9 @@
 
-function imageError(img, displayFunction) {
-	$(img).remove();
+function imageError(imagesPage, img) {
+	imagesPage.removeImage(img);	
 }
+
+ImagesPage.prototype._ImagesPage = 0;
 
 function ImagesPage(key, customSearchEngineIdentifier, elementWidth) {
 
@@ -11,79 +13,140 @@ function ImagesPage(key, customSearchEngineIdentifier, elementWidth) {
 	
 	elementWidth -= 20;	// #### I couldn't figure out why it was necessary to add this...
 	
+	this._images = Array();
+	this._errorTimeout = 0;
+	
+	
 		
 	this.search = function(searchStr, displayFunction) {
+
+		ImagesPage.prototype._ImagesPage = this;
+
+		
+		_this._errorTimeout = 0;	// clear this
 		
 		var numImages = 20;
-		var imageHeight = 150;
-		var minMargin = 5;
 		
 		var url = 'https://www.googleapis.com/customsearch/v1?key=' + key + '&cx=' + customSearchEngineIdentifier + '&q=' + 
 			searchStr + '&searchType=image&count=' + numImages + '&imgType=photo&format=json&callback=?';
 		$.getJSON(url, {},
 			function(result) {
-				var returnStr = '';
-				//_this.displayImages(result.items);
-				numImg = result.items.length;
 				
-				// for each row, get the total width occupied by images
-				// (this will be useful later when we determine how much margin to add between images)
-				var currentRow = 0;
-				var rowImageWidth = new Array();	// the current number of pixels occupied by image in this row
-				rowImageWidth[currentRow] = 0;
-				var numImages = new Array();		// the number of images in each row
-				numImages[currentRow] = 0;
-				var rowCurrWidth = minMargin;	// the current number of pixels (occupied by image or by margin) in this row
+				_this._images = result.items;
 				
-				for (var i = 0; i < numImg; i++, numImages[currentRow]++) {
-					var origWidth = result.items[i].image.thumbnailWidth;
-					var origHeight = result.items[i].image.thumbnailHeight;			
-					var newWidth = imageHeight * origWidth/origHeight;
-					
-					if (rowCurrWidth + newWidth > elementWidth) {
-						// no more room in this row
-						currentRow++;
-						rowImageWidth[currentRow] = 0;
-						numImages[currentRow] = 0;
-						rowCurrWidth = minMargin;
-					} 
-						
-					rowImageWidth[currentRow] += newWidth;
-					rowCurrWidth += newWidth + minMargin;
-					
-				}
-				
-				currentRow = 0;
-				var imagesThisRow = 0;
-				
-				for (var i = 0; i < numImg; i++) {
-					var origWidth = result.items[i].image.thumbnailWidth;
-					var origHeight = result.items[i].image.thumbnailHeight;			
-					var newWidth = imageHeight * origWidth/origHeight;
-					
-					// what is the width of the margin in this row?
-					var marginWidth = (elementWidth - rowImageWidth[currentRow]) / (numImages[currentRow] + 1);
-						
-					returnStr += '<img class="googleImage" onerror="imageError(this, ' + displayFunction + ')" src="' + result.items[i].link + '" ' +
-					'style="height:' + imageHeight + 'px;margin-left:' + marginWidth + 'px;margin-right:0px;margin-top:0px;margin-bottom:8px;padding:0px"/>';
-					
-				
-					imagesThisRow++;
-					if (imagesThisRow == numImages[currentRow]) {
-						currentRow++;
-						imagesThisRow = 0;
-						returnStr += '<br>';
-					}
-					
-				}
-				
-				this._html = returnStr;
+				_this._html = _this.getFormattedImages();
 				
 				if (displayFunction != null) {
-					displayFunction(this._html);
+					displayFunction(_this._html);
 				}
 			}
-		);
+		);	
+		
+	}
+	
+	
+	this.getFormattedImages = function(){
+		
+		var imageHeight = 150;
+		var minMargin = 5;
+		var returnStr = '';
+		
+		numImg = _this._images.length;
+		
+		// for each row, get the total width occupied by images
+		// (this will be useful later when we determine how much margin to add between images)
+		var currentRow = 0;
+		var rowImageWidth = new Array();	// the current number of pixels occupied by image in this row
+		rowImageWidth[currentRow] = 0;
+		var numImages = new Array();		// the number of images in each row
+		numImages[currentRow] = 0;
+		var rowCurrWidth = minMargin;	// the current number of pixels (occupied by image or by margin) in this row
+		
+		for (var i = 0; i < numImg; i++) {
+			
+			if (_this._images[i].link == '') {
+				continue;
+			}
+			
+			var origWidth = _this._images[i].image.thumbnailWidth;
+			var origHeight = _this._images[i].image.thumbnailHeight;			
+			var newWidth = imageHeight * origWidth/origHeight;
+			
+			if (rowCurrWidth + newWidth > elementWidth) {
+				// no more room in this row
+				currentRow++;
+				rowImageWidth[currentRow] = 0;
+				numImages[currentRow] = 0;
+				rowCurrWidth = minMargin;
+			} 
+				
+			rowImageWidth[currentRow] += newWidth;
+			rowCurrWidth += newWidth + minMargin;
+			numImages[currentRow]++;
+		}
+		
+		currentRow = 0;
+		var imagesThisRow = 0;
+		
+		for (var i = 0; i < numImg; i++) {
+			
+			if (_this._images[i].link == '') {
+				continue;
+			}
+			
+			var origWidth = _this._images[i].image.thumbnailWidth;
+			var origHeight = _this._images[i].image.thumbnailHeight;			
+			var newWidth = imageHeight * origWidth/origHeight;
+			
+			// what is the width of the margin in this row?
+			var marginWidth = (elementWidth - rowImageWidth[currentRow]) / (numImages[currentRow] + 1);
+			
+				
+			returnStr += '<img class="googleImage" src="' + _this._images[i].link + '" onerror="imageError(ImagesPage.prototype._ImagesPage, this)"' +
+			'style="height:' + imageHeight + 'px;width:' + newWidth + 'px;margin-left:' + marginWidth + 'px;margin-right:0px;margin-top:0px;margin-bottom:8px;padding:0px"/>';
+			
+		
+			imagesThisRow++;
+			if (imagesThisRow == numImages[currentRow]) {
+				currentRow++;
+				imagesThisRow = 0;
+				returnStr += '<br>';
+			}
+			
+		}
+		
+		return returnStr;
 	}
 	
 }
+
+
+
+ImagesPage.prototype.removeImage = function(img) {
+	
+	for (var i = 0; i < this._images.length; i++) {
+		if (this._images[i].link == img.src) {
+			this._images[i].link = '';		
+	
+			var _this = this;
+			
+			// set a timer to go off to refresh the page once we can be pretty sure that
+			// all image errors have been accounted for
+			if (_this._errorTimeout == 0) {
+				_this._errorTimeout = setTimeout(
+					function()
+					{
+						clearTimeout(_this._errorTimeout);
+						_this._html = _this.getFormattedImages();
+						//$(img).parent().html(_this._html);
+						var parent = $(img).parent('.tab-pane');
+						$(parent).html(_this._html);
+						//$('.tab-pane').html('blah');
+					}, 500);
+			}
+		}
+	}
+}
+
+
+
